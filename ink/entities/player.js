b2dab8299560c6088e9d35301a9b6fe3e5d2a1b6
Ross Hadden <rosshadden@gmail.com>
2012-04-26 12:21:53 -0400

    Basic animation working.

--- ink/entities/player.js
+++ ink/entities/player.js
@@ -1,28 +1,54 @@
 define(['./entity', 'engine/draw'], function(Entity, draw){
 	var Player = Entity.extend({
 		init:	function(properties){
-			this._super(properties);
+			var self = this;
 			
-			this.isAnimated = false;
+			self._super(properties);
 			
-			this.position = properties.position || {
+			self.isAnimated = true;
+			self.isAnimating = true;
+			
+			self.sprite.src = 'images/characters/cecil.png';
+			
+			self.position = properties.position || {
 				x:	0,
 				y:	0
 			};
 			
-			this.velocity = properties.velocity || {
+			self.velocity = properties.velocity || {
 				x:  4,
 				y:  4
 			};
 			
-			this.animation = [{
-				x:			0,
-				y:			0,
-				w:			100,
-				h:			100
+			self.animation = [{
+				x:			111,
+				y:			3,
+				w:			16,
+				h:			16
+			},{
+				x:			128,
+				y:			3,
+				w:			16,
+				h:			16
 			}];
 			
-			this.sprite.src = 'images/characters/cecil.png';
+			self.currentFrame = 0;
+			
+			self.interval = (function(interval){
+				var output;
+				if(typeof interval === 'number'){
+					output = function(frame){
+						return frame === interval;
+					};
+				}else if(typeof interval === 'boolean'){
+					output = function(){
+						return interval;
+					};
+				}else if(typeof interval === 'function'){
+					output = interval;
+				}
+				return output;
+			})(properties.interval || 6);
 		},
 		
 		move:	function(dir){
@@ -37,6 +63,15 @@ define(['./entity', 'engine/draw'], function(Entity, draw){
 				position:	this.position,
 				sprite:		this.animation[this.spriteIndex]
 			});
+			
+			if(this.isAnimating && this.interval(this.currentFrame)){
+				this.spriteIndex = (this.spriteIndex + 1) % this.animation.length;
+				this.currentFrame = 0;
+			}else if(!this.isAnimating){
+				this.currentFrame = 0;
+			}
+			
+			this.currentFrame++;
 		}
 	});
 	
