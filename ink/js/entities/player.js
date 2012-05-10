ed38c38332fab0d5da6807321326b39014676ef1
Ross Hadden <rosshadden@gmail.com>
2012-05-10 12:56:13 -0400

    Basic player movement.

--- ink/js/entities/player.js
+++ ink/js/entities/player.js
@@ -22,14 +22,43 @@ define(function(){
 			regX:	24 / 2,
 			regY:	24 / 2,
 			
+			speed:	2,
+			
+			moveTo:	function(x, y){
+				this.destinationX = x * engine.tile.sizeX + this.regX || this.posX;
+				this.destinationY = y * engine.tile.sizeY + this.regY || this.posY;
+			},
+			
 			update:	function(tick){
+				if(this.destinationX - this.posX > this.speed){
+					this.posX += this.speed;
+					this.animate('right');
+				}else if(this.destinationX - this.posX < -this.speed){
+					this.posX -= this.speed;
+					this.animate('left');
+				}else if(this.destinationY - this.posY > this.speed){
+					this.posY += this.speed;
+					this.animate('down');
+				}else if(this.destinationY - this.posY < -this.speed){
+					this.posY -= this.speed;
+					this.animate('up');
+				}else{
+					this.animate('idle');
+				}
 			}
 		}).init(function(){
 			this.posX = engine.sys.sizeX / 2;
 			this.posY = engine.sys.sizeY / 2;
 			
+			this.destinationX = this.posX;
+			this.destinationY = this.posY;
+			
 			this.animations = {
-				idle:	[100, [0, 1, 2, 3, 4], 100]
+				idle:	[100, [0, 1], 100],
+				up:		[100, [4, 5], 100],
+				down:	[100, [0, 1], 100],
+				left:	[100, [2, 3], 100],
+				right:	[100, [6, 7], 100]
 			};
 			
 			this.on('update', this.update);
--- ink/js/scenes/game.js
+++ ink/js/scenes/game.js
@@ -2,9 +2,14 @@ define(function(){
 	return function(engine){
 		engine.scene('game')
 		.enter(function(){
-			window.me = engine.e('player')
+			window.me = engine.e('player mouse')
 			.attr({
 				id:		'me'
+			}).on('click', function(x, y){
+				x = ~~(x / engine.tile.sizeX);
+				y = ~~(y / engine.tile.sizeY);
+				
+				this.moveTo(x, y);
 			});
 			
 			engine.utilities.log('Game entered.');
